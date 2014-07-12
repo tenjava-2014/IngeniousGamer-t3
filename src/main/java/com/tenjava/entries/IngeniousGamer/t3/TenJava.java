92d236a1e3832feed5dfa6f7750d731c272fbfbe
Bryce Howell <bryce@games-infinity.com>
2014-07-12 10:17:38 -0400

    Init push from environment

--- src/main/java/com/tenjava/entries/IngeniousGamer/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/IngeniousGamer/t3/TenJava.java
@@ -2,4 +2,11 @@ package com.tenjava.entries.IngeniousGamer.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	public void onEnable(){
+		//Start of Ten.Java
+	}
+	public void onDisable(){
+		
+	}
+}
