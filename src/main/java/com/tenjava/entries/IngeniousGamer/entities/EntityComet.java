0c3cf10e8c9f0dcb540d79a3811d09508aae1880
Bryce Howell <bryce@games-infinity.com>
2014-07-12 14:35:37 -0400

    Added Custom EntityComet to randomly fall and cause damage(Meteor Shower)

---
+++ src/main/java/com/tenjava/entries/IngeniousGamer/entities/EntityComet.java
@@ -0,0 +1,142 @@
+package com.tenjava.entries.IngeniousGamer.entities;
+
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.v1_7_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_7_R3.event.CraftEventFactory;
+import org.bukkit.craftbukkit.v1_7_R3.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import net.minecraft.server.v1_7_R3.Block;
+import net.minecraft.server.v1_7_R3.Blocks;
+import net.minecraft.server.v1_7_R3.EntityFallingBlock;
+import net.minecraft.server.v1_7_R3.Material;
+import net.minecraft.server.v1_7_R3.MathHelper;
+import net.minecraft.server.v1_7_R3.PacketPlayOutWorldParticles;
+import net.minecraft.server.v1_7_R3.World;
+import com.tenjava.entries.IngeniousGamer.util.Particles;
+
+public class EntityComet extends EntityFallingBlock{	  
+	private boolean f;
+	private double lastmotX;
+	private double lastmotY;
+	private double lastmotZ;
+	public static ArrayList<EntityComet> allcomets = new ArrayList<EntityComet>();
+	public EntityComet(World world, double d0, double d1, double d2,int bid,int bda) {
+	      super(world);
+	      this.dropItem = true;
+	      /* try{
+		    Method m = CraftBlock.class.getMethod("getNMSBlock", Integer.class);
+		    m.setAccessible(true);
+		    m.invoke(m, 1);
+		    
+			this.id = s;
+		    }catch(Exception e){
+		    	e.printStackTrace();
+		    }*/
+		    id = Block.a((CraftItemStack.asNMSCopy(new ItemStack(bid)).getItem()));
+		    
+		    data = bda;
+		    k = true;
+		    a(0.98F, 0.98F);
+		    height = (this.length / 2.0F);
+		    setPosition(d0, d1, d2);
+		    lastmotX = 0.0D;
+		    lastmotY = 0.0D;
+		    lastmotZ = 0.0D;
+		    motX = 0.0D;
+		    motY = 0.0D;
+		    motZ = 0.0D;
+		    lastX = d0;
+		    lastY = d1;
+		    lastZ = d2;
+		    allcomets.add(this);
+	   }
+	@Override
+	public void h() {
+		if (this.id.getMaterial() == Material.AIR) {
+			this.die();
+		} else {
+			this.lastX = this.locX;
+			this.lastY = this.locY;
+			this.lastZ = this.locZ;
+			lastmotX = this.motX;
+			lastmotY = this.motY;
+			lastmotZ = this.motZ;
+			
+			//Bukkit.broadcastMessage("loc");
+			++this.ticksLived;
+			this.motY -= 0.03999999910593033D;
+			//this.motY -= 0.00999910593033D;
+			//if(!checkCollision(this)){//move back into moving loop
+				this.setPosition(locX, locY, locZ);
+
+			this.move(this.motX, this.motY, this.motZ);
+			this.motX *= 0.9800000190734863D;
+			this.motY *= 0.9800000190734863D;
+			this.motZ *= 0.9800000190734863D;
+			//}
+			if (!this.world.isStatic) {
+				int i = MathHelper.floor(this.locX);
+				int j = MathHelper.floor(this.locY);
+				int k = MathHelper.floor(this.locZ);
+				
+				if (this.ticksLived == 1) {
+					// CraftBukkit - compare data and call event
+					if (this.ticksLived != 1 || this.world.getType(i, j, k) != this.id || this.world.getData(i, j, k) != this.data || CraftEventFactory.callEntityChangeBlockEvent(this, i, j, k, Blocks.AIR, 0).isCancelled()) {
+						this.die();
+						return;
+					}
+
+					this.world.setAir(i, j, k);
+				}
+				
+				if(this.motX == 0.0D && this.motZ != 0.0D){
+					this.motX = lastmotX*-1;
+					this.velocityChanged=true;
+					world.getWorld().playSound(new Location(world.getWorld(),locX,locY,locZ), Sound.DIG_WOOD, 2, 1);
+					
+				}
+				if(this.motZ == 0.0D && this.motX != 0.0D){
+					this.motZ = lastmotZ*-1;
+					this.velocityChanged=true;
+					world.getWorld().playSound(new Location(world.getWorld(),locX,locY,locZ), Sound.DIG_WOOD, 2, 1);
+				}
+				if(this.motY == 0.0D){
+					this.motY = lastmotY*-1;
+					this.velocityChanged=true;
+					world.getWorld().playSound(new Location(world.getWorld(),locX,locY,locZ), Sound.EXPLODE, 2, 1);
+					world.getWorld().createExplosion(new Location(world.getWorld(),locX,locY,locZ),3);
+					
+					allcomets.remove(this);
+					die();
+				}
+				if(Math.abs(motX) >= 0.01D || Math.abs(motZ) >= 0.01D || Math.abs(motY) >= 0.01D){
+		        	try {
+						Particles.sendParticle(Particles.FIREWORKS_SPARK, new Location(this.world.getWorld(),this.locX,this.locY,this.locZ), 0.2f, 0f, 0.2f, 0f, 5);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+		        	
+				}
+				else if(motX!=0.0D && motZ!=0.0D){
+					this.motX = 0.0D;
+					this.motZ = 0.0D;
+					this.velocityChanged=true;
+				}
+				
+				
+				
+				if (this.onGround) {
+				//	this.motX *= 0.699999988079071D;
+				//	this.motZ *= 0.699999988079071D;
+				//	this.motY *= -0.5D;
+
+				}	
+			}	
+		}	
+	}
+}
--- src/main/java/com/tenjava/entries/IngeniousGamer/t3/Initialize.java
+++ src/main/java/com/tenjava/entries/IngeniousGamer/t3/Initialize.java
@@ -1,21 +1,84 @@
 package com.tenjava.entries.IngeniousGamer.t3;
 
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import java.lang.reflect.Method;
+import java.util.Random;
 
+
+
+
+
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
+
+import com.tenjava.entries.IngeniousGamer.entities.EntityComet;
 import com.tenjava.entries.IngeniousGamer.util.RandomCollection;
 
 public class Initialize {
 	public static ItemStack melon = new ItemStack(Material.MELON_BLOCK, 2, (short)0);
 	public static ItemStack chestpiece = new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1, (short)0);
 	public static ItemStack diamond = new ItemStack(Material.DIAMOND, 1, (short)0);
-	public static ItemStack reward4 = new ItemStack(Material.GLASS, 1, (short)0);
-	public static ItemStack reward5 = new ItemStack(Material.GLASS, 1, (short)0);
+	public static ItemStack gold = new ItemStack(Material.GOLD_INGOT, 3, (short)0);
+	public static ItemStack iron = new ItemStack(Material.IRON_INGOT, 2, (short)0);
 
+	private static BukkitTask randomTask;
 	public static RandomCollection<String> items = new RandomCollection<String>();
 	public static void initEverything(){
 		items.add(1.0, melon);
 		items.add(0.1, diamond);
-		items.add(0.4, chestpiece);
+		items.add(0.2, chestpiece);
+		items.add(0.4, iron);
+		items.add(0.6, iron);
+		patch();
+		
+		
+		randomTask = Bukkit.getScheduler().runTaskTimer(TenJava.plugin, new Runnable(){
+			Random rand = new Random();
+			boolean check=true;
+			Player player = Bukkit.getOnlinePlayers()[rand.nextInt((Bukkit.getOnlinePlayers().length - (0)) + 1) + (0)];
+			public void run(){
+				if(check){
+					//cometMap.put(fr, tid); Add cometMap for tracking tasks when implemented
+					check=false;
+				}
+				Location cometSpawn = player.getLocation();
+				CraftWorld handle = (CraftWorld) cometSpawn.getWorld();
+				EntityComet fr= new EntityComet(handle.getHandle(),
+						cometSpawn.getX(),
+						cometSpawn.getY()+60,
+						cometSpawn.getZ(),
+						173,0);
+				handle.getHandle().addEntity(fr, SpawnReason.CUSTOM);
+				fr.ticksLived=1;
+				int randomZ = rand.nextInt((60 - (-60)) + 1) + (-60);
+				int randomY = rand.nextInt((0 - (0)) + 1) + (0);
+				int randomX = rand.nextInt((60 - (-60)) + 1) + (-60);
+				int dir;
+				if(randomX==1){dir = 1;}
+				else{dir = -1;}
+				//fr.setPosition(cometSpawn.getX()+(-60*dir), cometSpawn.getY()+10, cometSpawn.getZ()+randomZ);
+				//fr.motX=3.4*dir;
+				//fr.motY=1.2;
+				//fr.motZ=-(randomZ/5.8)*dir;
+			    fr.positionChanged=true;
+			    fr.velocityChanged=true;
+			}
+		}, 0, 200);
+	}
+	public static void patch() {
+		   try {
+		      Method a = net.minecraft.server.v1_7_R3.EntityTypes.class.getDeclaredMethod("a", Class.class, String.class, int.class);
+		      a.setAccessible(true);
+		      a.invoke(a, EntityComet.class, "Comet", 504);
+		   } catch (Exception ignored) {
+			  //Already Registered.
+		   }
 	}
 }
